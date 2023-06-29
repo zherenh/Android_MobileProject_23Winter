65173272dbe852e23f3583b6a684df453d3bed4a
zherenh <zherenh@student.unimelb.edu.au>
2023-06-29 15:47:51 +1000

    some_Bugs

--- app/src/main/java/com/example/android_mobile_link/MainActivity.java
+++ app/src/main/java/com/example/android_mobile_link/MainActivity.java
@@ -3,13 +3,39 @@ package com.example.android_mobile_link;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText height_edit, eight_edit;
+    TextView result_text;
+    Button calculate;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init_view();
     }
+
+    private void init_view(){
+        height_edit = findViewById(R.id.height);
+        eight_edit = findViewById(R.id.weight);
+        result_text = findViewById(R.id.result);
+        calculate = findViewById(R.id.calculate);
+        calculate.OnClickListener(actionBtnOnClick);
+
+    }
+
+    private View.OnClickListener actionBtnOnClick = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case.R.id.calculate:
+            }
+        }
+    }
+
 }
